de99fa5b588dd333bc31bccf7039aba57e122cf1
ChicRock <jungsungrak@gmail.com>
2018-09-02 19:28:40 +0900

    Wrapping a restapi with Graphql

--- graphql/db.js
+++ graphql/db.js
@@ -1,9 +1,24 @@
 import fetch from "node-fetch";
+import querystring from "querystring";
 
-const API_URL = "https://yts.am/api/v2/list_movies.json";
+const API_URL = "https://yts.am/api/v2/list_movies.json?";
 
-export const getMovies = async (limit, rating) => {
-  return await fetch(`${API_URL}`)
+export const getMovies = (limit, rating) => {
+  let REQUEST_URL = API_URL;
+
+  let params = {};
+
+  if (limit > 0) {
+    params.limit = limit;
+  }
+
+  if (rating > 0) {
+    params.minimum_rating = rating;
+  }
+
+  REQUEST_URL += querystring.stringify(params);
+
+  return fetch(REQUEST_URL)
     .then(response => response.json())
     .then(json => json.data.movies);
 };
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,8 +1,8 @@
-import { getMovies, getById, addMovie, deleteMovie } from "./db";
+import { getMovies } from "./db";
 
 const resolvers = {
   Query: {
-    movies: () => getMovies(),
+    movies: (_, { limit, rating }) => getMovies(limit, rating),
   },
 };
 
